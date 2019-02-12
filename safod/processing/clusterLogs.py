383a6a37f82fe3d4c6736a10993a81543e925859
jaredbryan881 <jared.bryan1998@gmail.com>
2019-02-11 20:44:21 -0700

    Updated docstrings. Added capability to transform back to native, unnormalized log space.

--- safod/processing/clusterLogs.py
+++ safod/processing/clusterLogs.py
@@ -5,22 +5,62 @@ import numpy as np
 
 class clusterLogs:
 	def __init__(self, data, logs):
+	    """Initializes log curve clustering by setting data and logs.
+	    
+	    Args:
+	        :param data: dict
+	            Dictionary containing log data and associated log names.
+	        :param logs: list
+	            List containing log names to cluster.
+	    """
 		self.data = data
 		self.logs = logs
 
 	def stackLogs(self):
+	    """Stack multiple log curves into a multidimensional numpy array.
+	    
+	    Returns:
+	        :return stackedLogs: np.array
+	            Numpy array containing stacked log data.
+	    """
 		logArr = [self.data[log] for log in self.logs]
 		stackedLogs = np.stack(logArr, axis=1)
 
 		return stackedLogs
 
 	def dbscan_cluster(self, data, eps, minSamples):
+	    """Cluster a dataset using the DBSCAN algorithm.
+	    
+	    Args:
+	        :param data: np.array
+	            Data containing log data in n dimensions.
+	        :param eps: float
+	            Maximum distance between two points to be considered as in the same neighborhood.
+	        :param minSamples: int
+	            Minimum number of points needed to define a cluster.
+	          
+	    Returns:
+	        :return db: DBSCAN object
+	            DBSCAN clustering object.
+	        :return clusterStats: dict
+	            Dictionary containing clustering statistics.
+	    """
 		db = DBSCAN(eps=eps, min_samples=minSamples).fit(data)
 
 		clusterStats = self.dbscan_cluster_stats(db)
 		return db, clusterStats
 			
 	def dbscan_cluster_stats(self, db):
+	    """Create dictionary for DBSCAN clustering statistics.
+	    
+	    Args: 
+	        :param db: DBSCAN object
+	            DBSCAN clustering object.
+	    
+	    Returns:
+	        :return clusterStats: dict
+	            Dictionary containing clustering statistics.
+	    """
 		clusterStats = {}
 		# labels
 		labels = db.labels_
@@ -42,9 +82,27 @@ class clusterLogs:
 
 
 def normalize(data, axis):
-	# Set all dataset ranges to [-1, 1]
+	"""Set all dataset ranges to [-1, 1].
+	
+	Args:
+	    :param data: np.array
+	        numpy array containing log data.
+	    :param axis: int
+	        axis to normalize over.
+	        
+	Returns:
+	    :return data: np.array
+	        numpy array containing normalized log data.
+	    :return nfList: list
+	        list of floats containing the factor used to normalize the data"""
+	nfList = []
 	for i in range(data.shape[axis]):
 		# get largest absolute value of an element
 		n_factor = np.max((abs(np.min(data[:, i])), abs(np.max(data[:, i]))))
 		data[:, i] /= n_factor
-	return data
+		
+		# append normalization factor to list
+		nfList.append(n_factor)
+		
+	return data, nfList
+
