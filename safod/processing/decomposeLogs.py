03cdec334682e504d3e52138b37ad5fe3913e89d
jaredbryan881 <jared.bryan1998@gmail.com>
2020-05-03 17:44:51 -0400

    Added ICA decomposition

--- safod/processing/decomposeLogs.py
+++ safod/processing/decomposeLogs.py
@@ -1,34 +1,56 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from sklearn.decomposition import PCA
+from sklearn.decomposition import PCA, FastICA
 from sklearn.preprocessing import MinMaxScaler
 
-def logsPCA(data, ncomponents, plot=False):
-	"""Decompose a 2D array of log data by PCA
+class logDecomposer:
+	def __init__(self, data, ncomponents):
+		# rescale data
+		scaler = MinMaxScaler(feature_range=[-1,1])
+		self.data = scaler.fit_transform(data)
+		self.ncomponents = ncomponents
 
-	Args:
-		:param data: np.array
-			Array containing log data
+	def logsPCA(self, plot=False):
+		"""Decompose a 2D array of log data by PCA
 
-	Returns:
-		:return log_pca: sklearn PCA object
-			PCA of the log data. Components and explained variance are attributes of this object.
-		:return log_trans: np.array
-			Fit and dimensionally reduced data matrix
-	"""
-	scaler = MinMaxScaler(feature_range=[-1, 1])
-	data_rescaled = scaler.fit_transform(data)
+		Args:
+			:param data: np.array
+				Array containing log data
 
-	log_pca = PCA().fit(data_rescaled)
+		Returns:
+			:return log_pca: sklearn PCA object
+				PCA of the log data. Components and explained variance are attributes of this object.
+			:return log_trans: np.array
+				Fit and dimensionally reduced data matrix
+		"""
+		log_pca = PCA().fit(self.data)
+		pca = PCA(n_components=self.ncomponents)
+		log_pca_trans = pca.fit_transform(self.data)
 
-	if plot:
-		plt.figure()
-		plt.plot(np.cumsum(log_pca.explained_variance_ratio_))
-		plt.xlabel('Number of Components')
-		plt.ylabel('Variance (%)') #for each component
-		plt.show()
-	
-	pca = PCA(n_components=ncomponents)
-	log_trans = pca.fit_transform(data_rescaled)
+		if plot:
+			plt.figure()
+			plt.plot(np.cumsum(log_pca.explained_variance_ratio_))
+			plt.xlabel('Number of Components')
+			plt.ylabel('Variance (%)') #for each component
+			plt.show()
 
-	return log_pca, log_trans
+		return log_pca, log_pca_trans
+
+	def logsICA(self):
+		"""Decompose a 2D array of log data by ICA
+
+		Args:
+			:param data: np.array
+				Array containing log data
+
+		Returns:
+			:return log_pca: sklearn FastICA object
+				ICA of the log data. Components and explained variance are attributes of this object.
+			:return log_trans: np.array
+				Fit and dimensionally reduced data matrix
+		"""
+		log_ica = FastICA().fit(self.data)
+		ica = FastICA(n_components=self.ncomponents)
+		log_ica_trans = ica.fit_transform(self.data)
+
+		return log_ica, log_ica_trans
